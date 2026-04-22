bf45334343c25178bed59d0e495252ee070dc337
Arne Rümmler <arne.ruemmler@gmail.com>
2026-04-22 13:16:23 +0200

    begin enrichment

---
+++ projectlibs/py/helpers/wikidata_utils.py
@@ -0,0 +1,11 @@
+# WIKIDATA_PROPERTIES = {
+#     "gnd": "P227",
+#     "factgrid": "P8168",
+#     "bionomia": "P6944",
+#     "saebi": "P1710",
+#     "start": "P571",
+#     "founder": "P112",
+#     "end": "P576",
+# }
+
+
